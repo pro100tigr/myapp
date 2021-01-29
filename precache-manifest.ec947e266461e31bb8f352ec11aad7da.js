20c32e3550d2230d8667d2f0d3d0759e1098f4b0
pro100tigr <fatsa135@mail.ru>
2021-01-29 21:38:50 +0600

    Updates

---
+++ precache-manifest.ec947e266461e31bb8f352ec11aad7da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f9b3cab35e0ab95ca448c13c732cd49",
+    "url": "./index.html"
+  },
+  {
+    "revision": "337d29a7cf713aadcbfa",
+    "url": "./static/css/2.fb75dcb6.chunk.css"
+  },
+  {
+    "revision": "337d29a7cf713aadcbfa",
+    "url": "./static/js/2.561946f5.chunk.js"
+  },
+  {
+    "revision": "9be8f09f0bfe1d20164d4c42e389b466",
+    "url": "./static/js/2.561946f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a32d37a319c10aa2b6e8",
+    "url": "./static/js/main.4223ffb6.chunk.js"
+  },
+  {
+    "revision": "5416c08adc152c0560ee",
+    "url": "./static/js/runtime-main.b3720eee.js"
+  }
+]);
